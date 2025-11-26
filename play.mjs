93b4169d772daa7e5698582057bd8d6b1d027866
k4 <pmh.ars14@gmail.com>
2025-11-27 00:42:41 +0700

    feat: dry run

--- play.mjs
+++ play.mjs
@@ -4,33 +4,49 @@ import robot from "@jitsi/robotjs";
 
 const KEY_MAP = {
 	48: "Z",
+	49: "Z-shift",
 	50: "X",
+	51: "C-control",
 	52: "C",
 	53: "V",
+	54: "V-shift",
 	55: "B",
+	56: "B-shift",
 	57: "N",
+	58: "M-control",
 	59: "M",
 
 	60: "A",
+	61: "A-shift",
 	62: "S",
+	63: "S-control",
 	64: "D",
 	65: "F",
+	66: "F-shift",
 	67: "G",
+	68: "G-shift",
 	69: "H",
+	70: "J-control",
 	71: "J",
 
 	72: "Q",
+	73: "Q-shift",
 	74: "W",
+	75: "E-control",
 	76: "E",
 	77: "R",
+	78: "R-shift",
 	79: "T",
+	80: "T-shift",
 	81: "Y",
+	82: "U-control",
 	83: "U",
 };
 
 const sleep = (ms) => new Promise((resolve) => setTimeout(resolve, ms));
 
 const main = async () => {
+	const dryRun = process.argv.includes("-n");
 	const filename = process.argv[2];
 	if (!filename) {
 		console.error("Usage: node play.mjs <midi-file>");
@@ -55,10 +71,10 @@ const main = async () => {
 	console.log(`Tempo: ${60000000 / microsecondsPerBeat} BPM`);
 	console.log(`Ticks per beat: ${ticksPerBeat}`);
 	console.log(`Starting in 3 seconds...`);
-	await sleep(3000);
+	!dryRun && await sleep(3000);
 
 	for (const event of midiArray.track[0].event) {
-		if (event.deltaTime > 0) {
+		if (!dryRun && event.deltaTime > 0) {
 			const delayMs = (event.deltaTime * microsecondsPerTick) / 1000;
 			await sleep(delayMs);
 		}
@@ -66,12 +82,15 @@ const main = async () => {
 		if (event.type !== 9) continue;
 
 		const noteNumber = event.data[0];
-		const key = KEY_MAP[noteNumber];
-		if (!key) {
+		const keys = KEY_MAP[noteNumber];
+		if (!keys) {
 			outliners.push(noteNumber);
 		} else {
-			robot.keyTap(key.toLowerCase());
-			console.log(key);
+			if (dryRun) continue;
+			
+			const [key, mod] = keys.split('-');
+			console.log(key, mod);
+			!!mod ? robot.keyTap(key.toLowerCase(), mod) : robot.keyTap(key.toLowerCase());
 		}
 	}
 
